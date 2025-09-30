7bd3e03550b110c28024fb3e5f95063500b45645
rahulreddy5 <rahulreddy1739@gmail.com>
2025-09-30 01:49:27 -0500

    initial project

---
+++ src/test/java/instant_order_project/instant_order/InstantOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package instant_order_project.instant_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
